57eb1b65f0d171f991f2b4c9e167c9ce55b6c0fb
nmstm <nathan.stmarie@mymail.champlain.edu>
2019-10-23 14:11:29 -0400

    Finished up for Week 9. Added functionality for: -Editing Passwords -Deleting Passwords -Password complexity requirements

--- password_saver.py
+++ password_saver.py
@@ -1,6 +1,6 @@
 import pickle
 import sys
-
+import re
 #The password list - We start with it populated for testing purposes
 entries = [["yahoo", "XqffoZeo"], ["google", "CoIushujSetu"]]
 #The password file name to store the data to
@@ -13,13 +13,15 @@ What would you like to do:
 1. Open password file
 2. Lookup a password
 3. Add a password
-4. Save password file
-5. Print the encrypted password list (for testing)
-6. Quit program
+4. Edit a password
+5. Delete a password
+6. Save password file
+7. Print the encrypted password list (for testing)
+8. Quit program
 Please enter a number (1-6)"""
 
 
-def password_encrypt (unencrypted_message, key):
+def password_encrypt(unencrypted_message, key):
     """Returns an encrypted message using a caesar cypher
 
     :param unencrypted_message (string)
@@ -30,36 +32,7 @@ def password_encrypt (unencrypted_message, key):
     #Fill in your code here.
     # If you can't get it working, you may want to put some temporary code here
     # While working on other parts of the program
-    encrypted_message = ""
-    """
-    Start with empty result
-    For each char in text
-        Find ordinal value
-        Add encryption key to ordinal value
-        While number is > 126 then subtract 94
-        While number is < 32 then add 94
-        Convert back to character value
-        Add result
-    ###################
-    min_limit = 32
-    max_limit = 126
-    For each char in text
-        Find ordinal value
-        new value = ordinal value - min_limit + encryption key
-        find remainder of new value divided by max_limit - min_limit + 1
-        add min_limit back in
-        convert back to char value
-        add result
-    
-    min_limit = 32
-    max_limit = 126
-    for char in unencrypted_message:
-        shifted_value = ord(char) - min_limit + key
-        num = (shifted_value % max_limit - min_limit) + 1
-        num += min_limit
-        encrypted_message += chr(num)
-    return encrypted_message
-    """
+
     encrypted_message = ""
     ord_list = []
     for char in unencrypted_message:
@@ -75,38 +48,30 @@ def password_encrypt (unencrypted_message, key):
     return encrypted_message
 
 
-print(password_encrypt("HappyJoy", 16))
-
-
-
-def get_ord_list(term):
-    ord_list = []
-    for char in term:
-        ord_list.append(ord(char))
-    return ord_list
-
-
-
-print(get_ord_list("HappyJoy"))
-print(get_ord_list("XqffoZeo"))
-print(get_ord_list(password_encrypt("HappyJoy", 16)))
 def load_password_file(file_name):
     """Loads a password file.  The file must be in the same directory as the .py file
 
     :param file_name (string) The file to load.  Must be a pickle file in the correct format
     """
-    with open(file_name, 'rb') as f:
-        loaded_entries, loaded_encryption_key = pickle.load(f)
-    global entries, encryption_key
-    entries = loaded_entries
-    encryption_key = loaded_encryption_key
+    try:
+        with open(file_name, 'rb') as f:
+            loaded_entries, loaded_encryption_key = pickle.load(f)
+        global entries, encryption_key
+        entries = loaded_entries
+        encryption_key = loaded_encryption_key
+    except:
+        print(f"Could not load the password file. Make sure the filename is {file_name}")
+
 
 def save_password_file(file_name):
     """Saves a password file.  The file will be created if it doesn't exist.
 
     :param file_name (string) The file to save.
     """
-    pickle.dump( (entries, encryption_key), open( file_name, "wb" ) )
+    try:
+        pickle.dump( (entries, encryption_key), open( file_name, "wb" ) )
+    except:
+        print("Could not save the file.")
 
 
 def add_entry(website, password):
@@ -127,6 +92,60 @@ def add_entry(website, password):
     encrypted_password = password_encrypt(password, encryption_key)
     new_entry = [website, encrypted_password]
     entries.append(new_entry)
+
+
+def edit_entry(website, password):
+    """Edits an existing entry's password
+
+    :param website (string) The website for the entry edit
+    :param password (string) The new password for the entry
+    """
+    encrypted_password = password_encrypt(password, encryption_key)
+    for i, entry in enumerate(entries):
+        if entry[0] == website:
+            entries[i][1] = encrypted_password
+
+
+def delete_entry(website):
+    """Deletes an existing entry
+    :param website (string) The website of the entry to delete
+    """
+    for i, entry in enumerate(entries):
+        if entry[0] == website:
+            entries.remove(entries[i])
+
+
+def check_complexity(password):
+    """Checks if the password meets complexity requirements.
+    :param password (string) The password being checked for complexity requirements.
+    :return: contstraint_string (string, empty if password is complex, descriptor if not)
+             isPasswordComplex (Boolean, self-explanatory)
+
+    Complexity requirements: password must be 12 char minimum length, must contain
+    at least one capital letter, at least one lowercase letter, at least two numbers, and at
+    least one special character (!@#$%^&*()_+).
+
+    This function requires "import re" (Regular Expression)
+    """
+    constraint_string = ""
+    isPasswordComplex = True
+
+    if (len(password) < 12):
+        constraint_string = "Password is not long enough. (min length is 12)."
+        isPasswordComplex = False
+    elif not re.search("\d{2}", password):
+        constraint_string = "Password has one or no digits (min. 2)."
+        isPasswordComplex = False
+    elif not re.search("[A-Z]", password):
+        constraint_string = "Password doesn't have any capital letters."
+        isPasswordComplex = False
+    elif not re.search("[a-z]", password):
+        constraint_string = "Password doesn't have any lowercase letters."
+        isPasswordComplex = False
+    elif not re.search("[!@#$%^&*()\-+]", password):
+        constraint_string = "Password has no special character (!@#$%^&*()_+)."
+        isPasswordComplex = False
+    return isPasswordComplex, constraint_string
 
 
 def lookup_password(website):
@@ -165,7 +184,13 @@ def lookup_password(website):
     else:
         return None
 
+"""
+I changed the behavior of the program a bit, to avoid the condition where
+there are multiple entries for the same website name. I made it so the user specifies
+an integer as the position of the entry to edit or delete.
 
+
+"""
 while True:
     print(menu_text)
     choice = input()
@@ -175,9 +200,12 @@ while True:
 
     if(choice == '2'): # Lookup at password
         print("Which website do you want to lookup the password for?")
-        for key_value in entries:
-            print(key_value[0])
-        website = input()
+        # Print all Website entries accompanied with index + 1 (for readability),
+        # then ask user to choose which index + 1 to edit
+        for i, key_value in enumerate(entries):
+            print(f"{i + 1}: {key_value[0]}")
+        website_index = int(input(f"Please enter a number (1-{len(entries)}): ")) - 1
+        website = entries[website_index][0]
 
         password = lookup_password(website)
         if password:
@@ -190,14 +218,47 @@ while True:
         website = input()
         print("What is the password?")
         unencrypted_password = input()
-        add_entry(website, unencrypted_password)
+        isPasswordComplex, returnString = check_complexity(unencrypted_password)
+        if isPasswordComplex:
+            add_entry(website, unencrypted_password)
+        else:
+            print(returnString)
+    if (choice == '4'):  # Edit a password
+        print("Which website's password do you want to edit? (Overwrite)")
+        for i, key_value in enumerate(entries):
+            print(f"{i + 1}: {key_value[0]}")
+        website_index = int(input(f"Please enter a number (1-{len(entries)}): ")) - 1
+        website = entries[website_index][0]
 
-    if(choice == '4'): #Save the passwords to a file
+        password = lookup_password(website)
+        if password:
+            new_password = input("Enter the new password: ")
+            isPasswordComplex, returnString = check_complexity(new_password)
+            if isPasswordComplex:
+                edit_entry(website, new_password)
+            else:
+                print(returnString)
+        else:
+            print("Website not found.")
+    if (choice == '5'): # Delete an entry
+        print("Which entry number do you wish to delete?")
+        for i, key_value in enumerate(entries):
+            print(f"{i+1}: {key_value[0]}")
+        website_index = int(input(f"Please enter a number (1-{len(entries)}): ")) - 1
+        website = entries[website_index][0]
+        password = lookup_password(website)
+
+        if password:
+            delete_entry(website)
+            print(f"Entry for {website} has been deleted.")
+        else:
+            print(f"Entry for {website} can't be deleted since it doesn't exist.")
+    if(choice == '6'): #Save the passwords to a file
             save_password_file(password_file_name)
 
-    if(choice == '5'): #print out the password list
+    if(choice == '7'): #print out the password list
         for key_value in entries:
             print(', '.join(key_value))
 
-    if(choice == '6'):  #quit our program
+    if(choice == '8'):  #quit our program
         sys.exit()
